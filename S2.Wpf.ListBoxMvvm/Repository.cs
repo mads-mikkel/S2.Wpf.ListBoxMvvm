ef2183c4f8bd756efdc1fe762c429d4be3db2ead
Mads Mikkel Rasmussen <mara@aspit.dk>
2020-01-23 10:49:42 +0100

    Created Repository with two initialized employees.

---
+++ S2.Wpf.ListBoxMvvm/Repository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace S2.Wpf.ListBoxMvvm
+{
+    public class Repository
+    {
+        private List<Employee> employees;
+
+        public Repository()
+        {
+            employees = new List<Employee>() {
+                new Employee() {Firstname = "Brian", Lastname = "Jørgensen", HireDate = new DateTime(01,08,2016), YearlySalary = 10000m},
+                new Employee() {Firstname = "Ole", Lastname = "Bay Jensen", HireDate = new DateTime(01,01,2005), YearlySalary = 12000m}
+            };
+        }
+
+        public List<Employee> GetAll()
+        {
+            return employees;
+        }
+
+        public void Add(Employee employee)
+        {
+            employees.Add(employee);
+        }
+    }
+}
