5a59a6e0499187a822c6bbe183b25f90f75c583b
Mads Mikkel Rasmussen <mara@aspit.dk>
2020-01-23 10:44:47 +0100

    Created Employee.

---
+++ S2.Wpf.ListBoxMvvm/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace S2.Wpf.ListBoxMvvm
+{
+    public class Employee
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public DateTime HireDate { get; set; }
+        public decimal YearlySalary { get; set; }
+
+        public string Fullname => $"{Firstname} {Lastname}";
+    }
+}
